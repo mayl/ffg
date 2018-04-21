c283e82dbe738a543f6d90accd42581f4497e9e6
LarryM <larrymayster@gmail.com>
2018-04-21 12:35:12 +0900

    fix bash version

--- ffg.sh
+++ ffg.sh
@@ -1,2 +1,3 @@
-alias ffg='fg $(jobs | hs | cut -f1 -d']' | cut -f2 -d'[' | fg)'
+alias ffg='fg $(jobs | hs | cut -f1 -d']' | cut -f2 -d'[')'
+alias fbg='bg $(jobs | hs | cut -f1 -d']' | cut -f2 -d'[')'
 
